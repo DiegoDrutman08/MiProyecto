51c2d7ea9b944a9e2fbf10679cd7693f28113353
Diego Drutman <diegod@logitec.com.ar>
2024-04-25 19:52:42 -0300

    changes models.py, urls.py

--- project/Clase/models.py
+++ project/Clase/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Comision(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True, blank=True)
+    profesor = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, blank=True)    
+    estudiante = models.ManyToManyField(Estudiante)
+ 
+    def __str__(self):
+        return str(self.nombre)
