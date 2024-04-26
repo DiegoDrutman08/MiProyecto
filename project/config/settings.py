e98882eafc9468da08793f0f2b42d7359d86fbab
Diego Drutman <diegod@logitec.com.ar>
2024-04-25 21:05:47 -0300

    changes: all

--- project/config/settings.py
+++ project/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'Clase',
 ]
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Mendoza'
 
 USE_I18N = True
 
--- project/config/urls.py
+++ project/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Clase.urls")),
+    path('', include("core.urls")),
+    path("comision/", include("Clase.urls"))
 ]
